11bb512d7d9b8fefdf940dc6a32360e0f9b213d7
Bibi Hajira <bibihajira@Bibis-MacBook-Air.local>
2024-03-19 15:26:44 -0400

    first-commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,26 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about-section">
+      <div className="about-section-img">
+        <img src="./img/about-me.png" alt="About Me " />
+      </div>
+      <div className="hero-section-content-box about-section-box">
+        <div className="hero-section-content">
+          <p className="section-title">About</p>
+          <h1 className="skills-section-heading"> About Me</h1>
+          <p className="hero-section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+            dolores ea pariatur esse aliquid quas modi neque in, doloremque
+            officia veniam distinctio facilis iusto! Consequuntur nesciunt
+            molestias voluptatibus qui! Quia.
+          </p>
+          <p className="hero-section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+            dolores ea pariatur esse aliquid quas modi neque in, doloremque
+            officia veniam distinctio facilis iusto!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
